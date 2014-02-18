d4fda71a616f0ba174d0c583c491851bdd07853b
Maxanier <maxanier@gmx.de>
2014-02-18 15:43:29 +0100

    Initial commit

---
+++ src/de/maxgb/minecraft/second_screen/SecondScreenMod.java
@@ -0,0 +1,18 @@
+package de.maxgb.minecraft.second_screen;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = SecondScreenMod.MODID, version = SecondScreenMod.VERSION)
+public class SecondScreenMod
+{
+    public static final String MODID = "SecondScreenMod";
+    public static final String VERSION = "0.1";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+}
