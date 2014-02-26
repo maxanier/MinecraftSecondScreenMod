5bb52ea4df0e4f336d04ebe1978691427091517b
Maxanier <maxanier@gmx.de>
2014-02-26 15:09:36 +0100

    Added disconnect command and changed simpleplayerinfolistener

--- src/de/maxgb/minecraft/second_screen/util/PROTOKOLL.java
+++ src/de/maxgb/minecraft/second_screen/util/PROTOKOLL.java
@@ -1,14 +1,17 @@
 package de.maxgb.minecraft.second_screen.util;
 
 public class PROTOKOLL {
-	public static final String REGISTER_S_PLAYERINFO_LISTENER = "r-spi"; // <username>
-	public static final String UNREGISTER_S_PLAYERINFO_LISTENER = "ur-spi"; // <username>
+	public static final String REGISTER_S_PLAYERINFO_LISTENER = "r-spil"; // <username>
+	public static final String UNREGISTER_S_PLAYERINFO_LISTENER = "ur-spil"; // <username>
 	public static final String UNREGISTER_ALL_LISTENER = "ur-all";
-	public static final String S_PLAYERINFO_LISTENER = "spi";
-	public static final String REGISTER_PLAYER_INVENTORY_LISTENER = "r-pil"; // <username>
-	public static final String UNREGISTER_PLAYER_INVENTORY_LISTENER = "ur-pil";// <username>
-	public static final String PLAYER_INVENTORY_LISTENER = "pil";
-	public static final String REGISTER_SERVER_INFO_LISTENER = "r-sil";
-	public static final String UNREGISTER_SERVER_INFO_LISTENER = "ur-sil";
-	public static final String SERVER_INFO_LISTENER = "sil";
+	public static final String S_PLAYERINFO_LISTENER="spil";
+	public static final String REGISTER_PLAYER_INVENTORY_LISTENER="r-pil"; // <username>
+	public static final String UNREGISTER_PLAYER_INVENTORY_LISTENER="ur-pil";// <username>
+	public static final String PLAYER_INVENTORY_LISTENER="pil";
+	public static final String REGISTER_SERVER_INFO_LISTENER="r-sil";
+	public static final String UNREGISTER_SERVER_INFO_LISTENER="ur-sil";
+	public static final String SERVER_INFO_LISTENER="sil";
+	public static final String DISCONNECT="disconnect";
+	
+	public static final String UNKNOWN="unknown";
 }
