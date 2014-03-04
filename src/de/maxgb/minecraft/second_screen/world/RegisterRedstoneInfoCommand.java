805b56e07a78561c757f6cf5ae5ce5d24b809cad
Maxanier <maxanier@gmx.de>
2014-03-04 18:38:40 +0100

    Redstone info command implementation - intial

---
+++ src/de/maxgb/minecraft/second_screen/world/RegisterRedstoneInfoCommand.java
@@ -0,0 +1,144 @@
+package de.maxgb.minecraft.second_screen.world;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.minecraft.command.ICommand;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.MathHelper;
+import net.minecraft.util.MovingObjectPosition;
+import net.minecraft.util.Vec3;
+
+public class RegisterRedstoneInfoCommand implements ICommand{
+
+	  private List aliases;
+	  public RegisterRedstoneInfoCommand()
+	  {
+	    this.aliases = new ArrayList();
+	    this.aliases.add("redstoneinfo");
+	    this.aliases.add("redinfo");
+	  }
+	  
+	@Override
+	public int compareTo(Object arg0) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getCommandName() {
+		return "redstoneinfo";
+	}
+
+	@Override
+	public String getCommandUsage(ICommandSender var1) {
+		return "redstoneinfo add <label>\nredstoneinfo remove <label>";
+	}
+
+	@Override
+	public List getCommandAliases() {
+		return aliases;
+	}
+
+	@Override
+	public void processCommand(ICommandSender var1, String[] var2) {
+		
+		if(var2.length < 2)
+	    {
+	      var1.addChatMessage(new ChatComponentText("Invalid arguments. Usage: "+getCommandUsage(var1)));
+	      return;
+	    }
+		
+		if(var2[0].equals("add")){
+			
+	        EntityPlayer player;
+	        
+	        if(var1 instanceof EntityPlayer){
+	                player = (EntityPlayer)var1;
+	        }
+	        else {
+	                var1.addChatMessage(new ChatComponentText("Player only command"));
+	                return;
+	        }
+	        
+
+			MovingObjectPosition p=getPlayerLookingSpot(player,true);
+			if(p==null){
+				var1.addChatMessage(new ChatComponentText("You have to look at a block"));
+				return;
+			}
+			
+			var1.addChatMessage(new ChatComponentText("You are looking at: "+p.blockX+","+p.blockY+","+p.blockZ));
+			
+			ObservingRegistry.observeBlock(var2[1], p.blockX, p.blockY, p.blockZ);
+			//var1.addChatMessage(new ChatComponentText(""+player.worldObj.isBlockIndirectlyGettingPowered(p.blockX, p.blockY, p.blockZ)));
+		}
+		else if(var2[0].equals("remove")){
+			
+		}
+		else{
+			var1.addChatMessage(new ChatComponentText("Invalid arguments. Usage: "+getCommandUsage(var1)));
+		      return;
+		      
+		}
+
+		
+	
+	}
+
+	@Override
+	public boolean canCommandSenderUseCommand(ICommandSender var1) {
+		if(var1.getCommandSenderName()=="Rcon"){
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public List addTabCompletionOptions(ICommandSender var1, String[] var2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isUsernameIndex(String[] var1, int var2) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	/**
+	 * Gets player´s looking spot.
+	 * @param player
+	 * @param restricts Keeps distance to players block reach distance
+	 * @return The position as a MovingObjectPosition, null if not existent
+	 * cf: https://github.com/ForgeEssentials/ForgeEssentialsMain/blob/master/src/main/java/com/forgeessentials/util/FunctionHelper.java
+	 */
+	public static MovingObjectPosition getPlayerLookingSpot(EntityPlayer player, boolean restrict)
+	{
+		float var4 = 1.0F;
+		float var5 = player.prevRotationPitch + (player.rotationPitch - player.prevRotationPitch) * var4;
+		float var6 = player.prevRotationYaw + (player.rotationYaw - player.prevRotationYaw) * var4;
+		double var7 = player.prevPosX + (player.posX - player.prevPosX) * var4;
+		double var9 = player.prevPosY + (player.posY - player.prevPosY) * var4 + 1.62D - player.yOffset;
+		double var11 = player.prevPosZ + (player.posZ - player.prevPosZ) * var4;
+		Vec3 var13 = player.worldObj.getWorldVec3Pool().getVecFromPool(var7, var9, var11);
+		float var14 = MathHelper.cos(-var6 * 0.017453292F - (float) Math.PI);
+		float var15 = MathHelper.sin(-var6 * 0.017453292F - (float) Math.PI);
+		float var16 = -MathHelper.cos(-var5 * 0.017453292F);
+		float var17 = MathHelper.sin(-var5 * 0.017453292F);
+		float var18 = var15 * var16;
+		float var20 = var14 * var16;
+		double var21 = 500D;
+		if (player instanceof EntityPlayerMP && restrict)
+		{
+		var21 = ((EntityPlayerMP) player).theItemInWorldManager.getBlockReachDistance();
+		}
+		Vec3 var23 = var13.addVector(var18 * var21, var17 * var21, var20 * var21);
+		return player.worldObj.rayTraceBlocks(var13, var23);
+		//return player.worldObj.rayTraceBlocks_do_do(var13, var23, false, !true);
+	}
+
+}
