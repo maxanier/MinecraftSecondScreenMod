6810317f5e2a159f2acf8649d75d8f67345f2ea1
Maxanier <maxanier@gmx.de>
2014-03-16 17:52:21 +0100

    Changed default configs

--- src/de/maxgb/minecraft/second_screen/Configs.java
+++ src/de/maxgb/minecraft/second_screen/Configs.java
@@ -1,15 +1,27 @@
 package de.maxgb.minecraft.second_screen;
 
+import java.net.Inet4Address;
+import java.net.UnknownHostException;
+
+import de.maxgb.minecraft.second_screen.util.Logger;
+
 import net.minecraftforge.common.config.Configuration;
 
 public class Configs {
 
 	public static String hostname;
 	public static int port;
+	private final static String TAG="Configs";
+	
 	public static void load(Configuration config) {
 		config.load();
-		hostname=config.get(Configuration.CATEGORY_GENERAL, "hostname", "localhost").getString();
-		port=config.get(Configuration.CATEGORY_GENERAL, "port", 25565).getInt();
+		try {
+			hostname=config.get(Configuration.CATEGORY_GENERAL, "hostname", Inet4Address.getLocalHost().getHostAddress()).getString();
+		} catch (UnknownHostException e) {
+			Logger.e(TAG, "Failed to retrieve host address"+e);
+			hostname="localhost";
+		}
+		port=config.get(Configuration.CATEGORY_GENERAL, "port", 25566).getInt();
 		config.save();
 	}
 
