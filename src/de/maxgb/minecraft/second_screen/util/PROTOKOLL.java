6c7aaf66446e0d17c9a100c230b710704e5b45c5
Maxanier <maxanier@gmx.de>
2014-03-26 19:33:19 +0100

    Register and unregister messages should now be build out of parts

--- src/de/maxgb/minecraft/second_screen/util/PROTOKOLL.java
+++ src/de/maxgb/minecraft/second_screen/util/PROTOKOLL.java
@@ -2,24 +2,17 @@ package de.maxgb.minecraft.second_screen.util;
 
 public class PROTOKOLL {
 
-	// Listener
+	//Listener managment commands
 	public static final String REGISTER_COMMAND_BEGIN="r-";
-	public static final String REGISTER_S_PLAYERINFO_LISTENER = "r-spil";
-	public static final String UNREGISTER_S_PLAYERINFO_LISTENER = "ur-spil";
-	public static final String UNREGISTER_ALL_LISTENER = "ur-all";
+	public static final String UNREGISTER_COMMAND_BEGIN="ur-";
+	
+	//Listeners
 	public static final String S_PLAYERINFO_LISTENER = "spil";
-	public static final String REGISTER_PLAYER_INVENTORY_LISTENER = "r-pil";
-	public static final String UNREGISTER_PLAYER_INVENTORY_LISTENER = "ur-pil";
 	public static final String PLAYER_INVENTORY_LISTENER = "pil";
-	public static final String REGISTER_SERVER_INFO_LISTENER = "r-sil";
-	public static final String UNREGISTER_SERVER_INFO_LISTENER = "ur-sil";
 	public static final String SERVER_INFO_LISTENER = "sil";
-	public static final String REGISTER_WORLD_INFO_LISTENER = "r-wil";
-	public static final String UNREGISTER_WORLD_INFO_LISTENER = "ur-wil";
 	public static final String WORLD_INFO_LISTENER = "wil";
 	public static final String CHAT_LISTENER = "cl";
-	public static final String REGISTER_CHAT_LISTENER = "r-cl";
-	public static final String UNREGISTER_CHAT_LISTENER ="ur-cl";
+	public static final String ALL_LISTENERS ="all";
 
 	// Connection
 	public static final String DISCONNECT = "disconnect";
