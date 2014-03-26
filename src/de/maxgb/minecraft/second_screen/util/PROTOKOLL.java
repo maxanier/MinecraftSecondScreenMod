18d3d805dc4c62fcbb40b38c1598327f6f528824
Maxanier <maxanier@gmx.de>
2014-03-26 19:35:35 +0100

    Formatting

--- src/de/maxgb/minecraft/second_screen/util/PROTOKOLL.java
+++ src/de/maxgb/minecraft/second_screen/util/PROTOKOLL.java
@@ -2,17 +2,17 @@ package de.maxgb.minecraft.second_screen.util;
 
 public class PROTOKOLL {
 
-	//Listener managment commands
-	public static final String REGISTER_COMMAND_BEGIN="r-";
-	public static final String UNREGISTER_COMMAND_BEGIN="ur-";
-	
-	//Listeners
+	// Listener managment commands
+	public static final String REGISTER_COMMAND_BEGIN = "r-";
+	public static final String UNREGISTER_COMMAND_BEGIN = "ur-";
+
+	// Listeners
 	public static final String S_PLAYERINFO_LISTENER = "spil";
 	public static final String PLAYER_INVENTORY_LISTENER = "pil";
 	public static final String SERVER_INFO_LISTENER = "sil";
 	public static final String WORLD_INFO_LISTENER = "wil";
 	public static final String CHAT_LISTENER = "cl";
-	public static final String ALL_LISTENERS ="all";
+	public static final String ALL_LISTENERS = "all";
 
 	// Connection
 	public static final String DISCONNECT = "disconnect";
@@ -30,7 +30,7 @@ public class PROTOKOLL {
 																// if success =
 																// 0,
 																// user(userobject))
-	public static final String ERROR="error";
+	public static final String ERROR = "error";
 
 	public static final String SERVER_STOPPING = "server_stopping";
 
