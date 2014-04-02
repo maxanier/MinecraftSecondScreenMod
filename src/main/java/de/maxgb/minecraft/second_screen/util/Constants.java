cc6eeb98f39b2ce140a07bba54be9375c9527b6a
Maxanier <maxanier@gmx.de>
2014-04-02 16:58:52 +0200

    Changed version

--- src/main/java/de/maxgb/minecraft/second_screen/util/Constants.java
+++ src/main/java/de/maxgb/minecraft/second_screen/util/Constants.java
@@ -3,7 +3,7 @@ package de.maxgb.minecraft.second_screen.util;
 public class Constants {
 
 	public static final String MOD_ID = "maxanier_secondscreenmod";
-	public static final String VERSION = "0.2.5";
+	public static final String VERSION = "0.2.6";
 	public static final String MINECRAFT_VERSION = "1.7";
 	public static final String NAME = "Second Screen Mod";
 
