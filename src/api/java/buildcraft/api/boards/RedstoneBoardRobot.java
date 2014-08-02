929ba1bc9c5dedb0eba77a3973c832e91968d216
Maxanier <maxanier@gmx.de>
2014-08-02 18:42:10 +0200

    Added Buildcraft api and updated Licence stuff

---
+++ src/api/java/buildcraft/api/boards/RedstoneBoardRobot.java
@@ -0,0 +1,61 @@
+/**
+ * Copyright (c) 2011-2014, SpaceToad and the BuildCraft Team
+ * http://www.mod-buildcraft.com
+ *
+ * BuildCraft is distributed under the terms of the Minecraft Mod Public
+ * License 1.0, or MMPL. Please check the contents of the license located in
+ * http://www.mod-buildcraft.com/MMPL-1.0.txt
+ */
+package buildcraft.api.boards;
+
+import java.util.HashSet;
+
+import buildcraft.api.core.BlockIndex;
+import buildcraft.api.robots.AIRobot;
+import buildcraft.api.robots.EntityRobotBase;
+
+public abstract class RedstoneBoardRobot extends AIRobot implements IRedstoneBoard<EntityRobotBase> {
+
+	public static HashSet<BlockIndex> reservedBlocks = new HashSet<BlockIndex>();
+
+	public RedstoneBoardRobot(EntityRobotBase iRobot) {
+		super(iRobot);
+	}
+
+	@Override
+	public abstract RedstoneBoardRobotNBT getNBTHandler();
+
+	@Override
+	public final void updateBoard(EntityRobotBase container) {
+
+	}
+
+	// TODO: we should put the three calls below into one object making sure
+	// that blocks are released before being assigned again, and which has a
+	// finalize () method to free potential block upon garbage collection.
+	public static boolean isFreeBlock(BlockIndex index) {
+		synchronized (reservedBlocks) {
+			return !reservedBlocks.contains(index);
+		}
+	}
+
+	public static boolean reserveBlock(BlockIndex index) {
+		synchronized (reservedBlocks) {
+			if (!reservedBlocks.contains(index)) {
+				reservedBlocks.add(index);
+				return true;
+			} else {
+				return false;
+			}
+		}
+	}
+
+	public static void releaseBlock(BlockIndex index) {
+		synchronized (reservedBlocks) {
+			if (reservedBlocks.contains(index)) {
+				reservedBlocks.remove(index);
+			}
+		}
+	}
+
+}
